ae307b0bd30606e5f061a3abd189d72af22fcbb4
Lotte van den Berg <lr_vandenberg@hotmail.com>
2019-05-02 00:01:08 +0200

    final improvements

--- homework/Week_3/week3.js
+++ homework/Week_3/week3.js
@@ -1,9 +1,11 @@
 /*
 Name: Lotte van den Berg
 Student number: 12427241
-This file provides the JavaScript code to plot a line chart.
+This file provides the JavaScript code to plot a line chart
+of the US births from 2000-2014.
 */
 
+// Add filename for GET request
 var fileName = "US_births_2000-2014_SSA.json";
 
 // Send XMLHttpRequest
@@ -19,17 +21,18 @@ txtFile.onreadystatechange = function() {
       // Preprocess data to workable {x: , y:} format
       var datapoints = preprocess(data);
 
-      // Access the drawing context of the canvas element with id=linechart
+      // Access the drawing context of the canvas element by its #id
       const ctx = document.getElementById('linechart').getContext('2d');
-      const canvas = document.getElementById('linechart')
+      const canvas = document.getElementById('linechart');
 
       // Draw the line chart
       draw(ctx, canvas, datapoints);
-    }
-}
+    };
+};
 txtFile.open("GET", fileName);
 txtFile.send();
 
+// Functions
 function preprocess(data){
   // Save keys in a seperate array for easy access
   var dataKeys = Object.keys(data);
@@ -37,30 +40,36 @@ function preprocess(data){
   // Put coordinates in right format
   var dataformat = [];
   for (var i = 0; i < 15; i++) {
+    // Convert strings to numbers, devide births by 1000 for better visuality
     dataformat.push({'x': Number(dataKeys[i]), 'y': data[dataKeys[i]]['births']/1000});
   };
 
+  // Return array with JSON objects
   return dataformat;
 };
 
 function draw(ctx, canvas, datapoints) {
-  // Set ranges for the line chart
+  // Set ranges for the linechart
+  // Padding is the necessary space for writing around the ranges of the linechart
   var padding = 50;
   var xRange = [0 + padding, canvas.width - padding];
-  // yRange starts with max value, so that the origin of the chart is in the left bottom corner
+  // yRange starts with max value, so the origin of the chart will be in the left bottom corner
   var yRange = [canvas.height - padding, 0 + padding];
 
-  // Draw title
-  ctx.textAlign="center"
+  // Draw title of the linechart
+  ctx.textAlign = "center";
+  ctx.font = "20px Georgia";
   ctx.fillText('US births 2000-2014', canvas.width/2, padding/2);
 
   // Draw x-label
-  ctx.textAlign="right"
+  ctx.textAlign="right";
+  ctx.font = "12px Georgia";
   ctx.fillText('Year', canvas.width - padding, canvas.height - padding/2);
 
   // Draw y-label
+  // Translate and rotate to write vertically
   ctx.save();
-  ctx.translate(20, 50);
+  ctx.translate(padding/2, padding);
   ctx.rotate(-Math.PI / 2);
   ctx.fillText("Births x 1000", 0, 0);
   ctx.restore();
@@ -68,7 +77,6 @@ function draw(ctx, canvas, datapoints) {
   // Draw x-axis and y-axis
   ctx.beginPath();
   ctx.lineWidth = 1;
-  // 0 is minimum range, 1 is maximum range (see ranges above)
   ctx.moveTo(xRange[0], yRange[1]);
   ctx.lineTo(xRange[0], yRange[0]);
   ctx.lineTo(xRange[1], yRange[0]);
@@ -76,41 +84,49 @@ function draw(ctx, canvas, datapoints) {
   ctx.closePath();
 
   // Linear functions for x and y
-  var xDomain = [datapoints[0].x, datapoints[14].x];
-  var yDomain = [0, 15]
+  // x min is first year, x max is last year of the datapoints array
+  var xDomain = [datapoints[0].x, datapoints[datapoints.length - 1].x];
+  // Most y-coordates are between 10-15, so the y domain should be 0-15
+  var yDomain = [0, 15];
   var xLine = createTransform(xDomain, xRange);
   var yLine = createTransform(yDomain, yRange);
 
   // Draw a line from datapoint to datapoint
   ctx.beginPath();
   ctx.lineWidth = 3;
+  // Create a variable to wright below the x-axis
   var yWrite = yRange[0] + 10;
+  // Write first x value below x-axis
   ctx.fillText(datapoints[0].x, xRange[0], yWrite);
+  // Start drawing the line: move to first datapoint
   ctx.moveTo(xLine(datapoints[0].x), yLine(datapoints[0].y));
   for (var i = 1; i < 15; i++) {
     var xNew = xLine(datapoints[i].x);
     var yNew = yLine(datapoints[i].y);
-    console.log(xNew, yNew);
+    // Move to the new datapoint
     ctx.lineTo(xNew, yNew);
-    ctx.fillText(datapoints[i].x, xNew, yWrite)
+    // Write x-value below x-axis
+    ctx.fillText(datapoints[i].x, xNew, yWrite);
   };
+  // Visualize the line in blue
+  ctx.strokeStyle = 'blue';
   ctx.stroke();
 
-  // Draw y-values
+  // Draw y-values left of y-axis
+  // Create a variable to write left of the y-axis
+  var xWrite = xRange[0] - 5;
   for (var i = yDomain[0]; i <= yDomain[1]; i += 5) {
-    ctx.fillText(i, 45, yRange[0] - 10 * i)
-  }
+    ctx.fillText(i, xWrite, yRange[0] - 10 * i);
+  };
 
-}
+};
 
 function createTransform(domain, range){
-	// range_min = alpha * domain_min + beta
-	// range_max = alpha * domain_max + beta
+	// Range_min = alpha * domain_min + beta
+	// Range_max = alpha * domain_max + beta
   var alpha = (range[1] - range[0]) / (domain[1] - domain[0]);
   var beta = range[1] - alpha * domain[1];
-  console.log(alpha);
-  console.log(beta);
-  // returns the function for the linear transformation (y= a * x + b)
+  // Returns the function for the linear transformation (y= a * x + b)
   return function(x){
     return alpha * x + beta;
   };
