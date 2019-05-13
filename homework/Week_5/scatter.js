ce66045d12c1471af3f6d6eb4bf8df65223e613d
Lotte van den Berg <lr_vandenberg@hotmail.com>
2019-05-13 11:01:41 +0200

    starting files

---
+++ homework/Week_5/scatter.js
@@ -0,0 +1,11 @@
+/*
+Name: Lotte van den Berg
+Student number: 12427241
+This file provides the JavaScript code to compute a scatterplot
+of the[......].
+*/
+
+window.onload = function() {
+
+  console.log('Yes, you can!')
+};
